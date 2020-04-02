39a2031438bf2caf5f0c3956f455d70b911ca342
DINH Son-Michel <son-michel.dinh@insa-lyon.fr>
2020-04-02 11:45:22 +0200

    Added OurString.h and OurString.cpp

---
+++ OurString.cpp
@@ -0,0 +1,64 @@
+#include "OurString.h"
+
+// Student A
+OurString::OurString(OurString &string)
+{
+
+};
+
+char* OurString::c_str()
+{
+
+};
+
+size_t OurString::size()
+{
+
+};
+
+void OurString::clear()
+{
+
+};
+
+OurString OurString::operator=(char c)
+{
+
+};
+
+OurString OurString::operator+(const string&, const char* s)
+{
+
+};
+
+
+// Student B
+OurString::OurString(char &string)
+{
+
+};
+
+size_t OurString::length()
+{
+
+};
+
+size_t OurString::max_size()
+{
+
+};
+
+void OurString::resize(size_t n, char c)
+{
+
+};
+
+OurString OurString::operator=(const string&)
+{
+
+};
+
+OurString OurString::operator+(const string&, char c)
+{
+
+};
