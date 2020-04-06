4bb51094951f3c675bdaadc8ad34f66013f6711a
DINH Son-Michel <son-michel.dinh@insa-lyon.fr>
2020-04-06 16:22:06 +0200

    Working gtest

--- OurStringTest.cpp
+++ OurStringTest.cpp
@@ -4,5 +4,16 @@
 
 TEST(GTestTests, FirstTest) {
 	OurString string;
-	EXPECT_EQ(string.c_str(),"Hello World");
+	bool check = true;
+	char* c_string = string.c_str();
+	char hello[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r','l', 'd', '\0'};
+	short i = 0;
+	while(c_string[i] != '\0' && hello[i] != '\0'){
+		if (c_string[i] != hello[i])
+		{
+			check = false;
+		}
+		i++;
+	}
+	EXPECT_TRUE(check);
 }
